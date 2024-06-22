febc4e96f1e3a76755f58a407d625a292a550301
Jayash Saini <jayashysaini7361@gmail.com>
2024-06-22 21:34:01 +0530

    feat(card): redesign card layout

--- src/Components/Recommendation/Card.jsx
+++ src/Components/Recommendation/Card.jsx
@@ -8,7 +8,7 @@ import hospitalImage from "../../assets/Images/Recommendation/Rectangle4185.png"
 
 const Card = ({ hospital, onCallNow, onBookAppointment }) => {
   return (
-    <div className="w-full flex flex-col gap-2 h-auto border-2 border-teal-400 rounded-xl px-3 py-5">
+    <div className="w-full flex flex-col gap-2 h-auto border-2 border-teal-400 rounded-xl px-2 py-5">
       {/* Left Hospital Image Section */}
       <div className="w-full h-[200px] rounded-xl overflow-hidden">
         <img
@@ -19,39 +19,39 @@ const Card = ({ hospital, onCallNow, onBookAppointment }) => {
       </div>
       {/* Right Description Section */}
       <div className="w-full h-full ">
-        <h2 className="text-lg font-semibold text-slate-700">
+        <h2 className="text-base font-semibold text-slate-700">
           {hospital.name} - {hospital.title}
         </h2>
         <div className="flex w-full justify-start gap-3 my-2">
-          <div className="flex items-center border-[1px] p-2 px-4 gap-6 rounded-md shadow-sm border-gray-300">
-            <img src={bedImage} alt="Bed" className="w-7 h-7" />
-            <p className="ml-2">{hospital.beds}</p>
+          <div className="flex items-center border-[1px] px-3 py-1 gap-6 rounded-md shadow-sm border-gray-300">
+            <img src={bedImage} alt="Bed" className="w-5 h-5" />
+            <p className="ml-2 text-sm">{hospital.beds}</p>
           </div>
-          <div className="flex items-center border-[1px] p-2 px-4 gap-6 rounded-md shadow-sm border-gray-300">
-            <img src={doctorsImage} alt="Doctors" className="w-7 h-7" />
-            <p className="ml-2">{hospital.doctors}</p>
+          <div className="flex items-center border-[1px] px-3 gap-6 rounded-md shadow-sm border-gray-300">
+            <img src={doctorsImage} alt="Doctors" className="w-5 h-5" />
+            <p className="ml-2 text-sm">{hospital.doctors}</p>
           </div>
         </div>
-        <div className="mt-5 flex gap-3">
-          <p className="flex gap-1 items-center text-sm text-gray-700">
+        <div className="mt-2 flex gap-3">
+          <p className="flex gap-1 items-center text-xs text-gray-700">
             <img src={nearestImage} alt="Nearest" className="w-4 h-4" />{" "}
             {hospital.near}
           </p>
-          <p className="text-sm text-gray-700">{hospital.stateCity}</p>
+          <p className="text-xs text-gray-700">{hospital.stateCity}</p>
         </div>
         <div className="my-2">
-          <p className="flex gap-1 items-center text-sm text-gray-700">
+          <p className="flex gap-1 items-center text-xs text-gray-700">
             <img src={timeImage} alt="Time" className="w-4 h-4" />{" "}
             {hospital.openingTime} - {hospital.closingTime}
           </p>
         </div>
-        <div className="my-2">
-          <p className="flex gap-1 items-center text-sm text-gray-700">
+        <div className="">
+          <p className="flex gap-1 items-center text-xs text-gray-700">
             <img src={locationImage} alt="Location" className="w-4 h-4" />{" "}
             {hospital.location}
           </p>
         </div>
-        <div className="w-full mt-3 flex gap-2">
+        <div className="w-full mt-2 flex gap-2">
           <button
             className="w-full px-5 py-2 border-[1px] border-teal-400 text-xs text-teal-400 rounded-lg shadow-lg hover:bg-teal-400 ease-in duration-200 hover:text-white"
             onClick={onCallNow}
